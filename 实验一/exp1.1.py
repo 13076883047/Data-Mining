52333df37efe068130722099803591450225551a
username <2412971288@qq.com>
2020-12-25 13:51:37 +0800

    合并数据
    合并txt和xlsx 成 csv 文件

---
+++ 实验一/exp1.1.py
@@ -0,0 +1,175 @@
+##  两个数据源合并后读入内存，并统计
+
+from numpy.core.defchararray import index
+import pandas as pd        # python 数据分析模块
+import numpy as np                                       
+import math
+pd.set_option('display.max_rows',None)
+
+# 性别统一： 男(boy)->male  女(girl)->female
+def gen(gender):
+    if gender == 'boy':
+        return 'male'
+    elif gender == 'girl':
+        return 'female'
+    else:
+        return gender
+
+
+# 体测成绩百分制
+def peScore(pe):
+    if pe == 'excellent':
+        return 90
+    elif pe == 'good':
+        return 80
+    elif pe =='general':
+        return 70
+    elif pe =='bad':
+        return 60
+    elif pe=='':
+        return np.NaN
+
+
+# 身高统一使用 米 为单位
+def height(height):
+    h = float(height)
+    if h>100:              # 厘米为单位的身高除以100
+        h=h/100.0
+        return h
+    else:
+        return h
+
+# C6-C10 十分制课程 转 百分制
+def tenTimes(score):
+    if score=='':
+        return score
+    s=float(score)*10
+    return s
+
+def tptoPer(df):
+    df['C6'] =df['C6'].apply(tenTimes)
+    df['C7'] =df['C7'].apply(tenTimes)
+    df['C8'] =df['C8'].apply(tenTimes)
+    df['C9'] =df['C9'].apply(tenTimes)
+    df['C10'] =df['C10'].apply(tenTimes)
+
+
+# 合并处理函数
+def dupMerge(dataframe):
+    '''
+    合并处理函数,仅保留重复的第一个值,如果行中有空缺则向重复行寻找
+    '''
+    df=dataframe          # dataframe 以命名列方式的分布式数据集
+
+    # 根据IP排序
+    # DataFrame.sort_values（by，axis = 0，ascending = True，inplace = False，kind = ' quicksort '，na_position = 'last'，ignore_index = False，key = None）
+
+    df.sort_values(by='ID',inplace = True, ascending = True)   # 按照某一列的大小进行升序排序
+
+    # 去除完全重复的行
+    df = df.drop_duplicates(keep='first')    # 保留第一次出现的数据
+
+    # 重设index
+    df = df.reset_index(drop=True)
+
+    # 定位'ID'重复的位置，位置存入 dupStartList=>List
+    idcol = df['ID']
+    oneDup = idcol.drop_duplicates(keep='first')    # 保存第一次出现的重复行
+    notDup = idcol.drop_duplicates(keep = False)    # 删除所有重复项
+    dupStart = oneDup.append(notDup).drop_duplicates(keep=False)
+
+    # 所有重复数据的起始处保存为列表List
+    dupStartList = dupStart.index.tolist()
+
+    # 取列数
+    col = df.shape[1]
+
+    # 处理'ID'重复的数据
+    for row in dupStartList:
+        dupStart = opFlag = row
+        dupEnd = row
+        # 找到重复区间，dupEnd为重复结束index
+        while df.iloc[dupEnd,0] == df.iloc[dupEnd+1,0]:
+            dupEnd = dupEnd + 1
+        # 如果出现空值，则向下寻找非空值
+        for c in range(col):
+            if pd.isna(df.iloc[dupStart,c]):
+                while dupEnd - opFlag >0:
+                    opFlag = opFlag + 1
+                    if not pd.isna(df.iloc[opFlag,c]):
+                        df.iloc[dupStart,c] = int(df.iloc[opFlag,c])
+                        break
+
+    # 处理结束后，去除其他重复行
+    df = df.drop_duplicates(subset=['ID'],keep = 'first')
+
+    # 重设index
+    df = df.reset_index(drop = True)
+
+    return df
+
+
+ ## 返回列表的平均值，计算时跳过空缺值
+def mean_list(list)->float:  
+    sum = float(0)
+    n = float(len(list))
+    for num in list:
+        # 跳过空缺值
+        if math.isnan(num):
+            n-=1
+            continue
+        sum += num
+    # list 中没有有效值
+    if n==0:
+        return float('nan')
+    mean = sum / n
+    return mean
+
+    
+def fillNaN(df):
+    cLabels = df.columns.values.tolist()[5:16]
+    for label in cLabels:
+        cList =df[label].tolist()
+        cMean =mean_list(cList)
+        if math.isnan(cMean):
+            continue
+        df[label].fillna(cMean,inplace = True)
+
+
+if __name__ =='__main__':
+    converters={'Gender':lambda x:gen(x),'Height':lambda x: height(x),'Constitution':lambda x:peScore(x)}
+
+    # df1
+    df1 = pd.read_table('D:\大三上\数据挖掘\data2.txt',sep=',',converters=converters)
+    
+    tptoPer(df1)
+    df1 =dupMerge(df1)
+
+    # df2
+    df2 = pd.read_excel('D:\大三上\数据挖掘\data1.xlsx',converters=converters)
+
+    # 学生ID处理函数
+    def compID(id):
+        x = int(id)
+        return 202000+x
+
+    df2['ID'] = df2['ID'].apply(compID)
+    tptoPer(df2)
+    df2=dupMerge(df2)
+
+
+    # df
+    # 合并两个表
+    df = pd.concat([df1,df2],ignore_index=True)
+    df = dupMerge(df)
+
+    # 将空缺处填上该列均值
+    fillNaN(df)
+
+    # 最终合并的dataframe => 'df
+    print(df)
+
+    #将结果保存到csv中
+    df.to_csv('D:\大三上\数据挖掘\data.csv',index=False)
+
+    print("Done")
---
+++ 实验一/exp1.2.py
@@ -0,0 +1,135 @@
+import pandas as pd
+import copy
+
+def question_1(data,count):     # 家乡为Beijing的所有课程的平均成绩
+    answer1 = []     # 结果
+    temp = []        # 暂存学生数据
+    i = 0
+    while i < count:
+        if data.loc[i]['City'] == 'Beijing':
+            temp.append(data.loc[i]['ID'])
+            temp.append(data.loc[i]['Name'])
+            aver_Grade = (data.loc[i]['C1'] + data.loc[i]['C2'] + data.loc[i]['C3'] + data.loc[i]['C4'] + data.loc[i]['C5'] +
+                          data.loc[i]['C6'] + data.loc[i]['C7'] + data.loc[i]['C8'] + data.loc[i]['C9'] + data.loc[i]['C10']) / 10
+            temp.append(aver_Grade)
+            a = copy.deepcopy(temp)
+            answer1.append(a)
+            temp.clear()
+        i += 1
+    print("家乡为Beijing的学生所有课程的平均成绩：\n", answer1)
+
+
+def question_2(data,count):
+    #学生中家乡在广州，课程1在80分以上，且课程9在9分以上的男同学的数量---问题二
+    boy_Gz = 0
+    i = 0
+    while i < count:   
+        if d.loc[i]['City'] == 'Guangzhou' and d.loc[i]['Gender']=='male' and d.loc[i]['C1'] > 80 and d.loc[i]['C9']>90:
+            boy_Gz += 1
+        i += 1
+    print("家乡在广州，课程1在80分以上，且课程9在9分以上的男同学的数量:",boy_Gz)
+
+
+
+def question_3(data,count):  # 比较广州和上海两地女生的平均体能测试成绩，哪个地区的更强
+    count_Gz = 0      # 统计广州女生人数
+    count_Sh = 0      # 统计上海女生人数
+    Grade_Gz = 0      # 广州女生的体育成绩
+    Grade_Sh = 0      # 上海女生的体育成绩
+    
+    i = 0
+    while i < count:
+        if d.loc[i]['City'] == 'Guangzhou' and d.loc[i]['Gender'] == 'female':
+            count_Gz += 1
+            Grade_Gz += d.loc[i]['Constitution']
+        if d.loc[i]['City'] == 'Shanghai' and d.loc[i]['Gender'] == 'female':
+            count_Sh += 1
+            Grade_Sh += d.loc[i]['Constitution']
+        i += 1
+    aver_Gz = Grade_Gz / count_Gz
+    aver_Sh = Grade_Sh / count_Sh
+    print("广州女生平均体能测试成绩:",aver_Gz)
+    print("上海女生平均体能测试成绩:",aver_Sh)
+    if aver_Gz > aver_Sh:
+        print("广州女生的平均体能测试成绩更好")
+    elif aver_Sh > aver_Gz:
+        print("上海女生的平均体能测试成绩更好")
+    else:
+        print("广州上海女生平均体能成绩一样好")
+
+def mean(data,lesson):   # 求平均值
+    sum = 0
+    row = data.shape[0]
+    for i in range(row):
+        sum += data[lesson][i]
+    mean_sum = sum / row   # 设置精度
+    return mean_sum
+
+def SD(data,lesson):  # 求标准差
+    sd_x = 0         #协方差
+    sd_b = 0         #标准差
+    sum_xi = 0      
+    sum_xi_2 = 0 
+    
+    row =data.shape[0]
+    for i in range(row):
+        sum_xi += data[lesson][i]
+        sum_xi_2 += data[lesson][i]**2
+
+    sd_x = (sum_xi_2-sum_xi*sum_xi/(row+1)) / row 
+    sd_b = sd_x ** 0.5 
+
+    return sd_b
+
+def ak(data,lesson):
+    row = data.shape[0]
+    list_a = {}
+    mean_sum = mean(data,lesson)
+    sd_sum = SD(data,lesson)
+    for i in range(row):
+        list_a[i] = (data[lesson][i] - mean_sum) / sd_sum
+
+    return list_a
+
+def bk(data):
+    row = data.shape[0]
+    list_b = {}
+    mean_sum = mean(data,'Constitution')
+    sd_sum = SD(data,'Constitution')
+    for i in range(row):
+        list_b[i] = (data['Constitution'][i]-mean_sum) / sd_sum
+
+    return list_b
+
+def question_4(data,lesson):      # 学习成绩和体能成绩测试，两者相关性是多少，九门课的成绩分别与体能成绩计算相关性
+    dependence_sum = 0
+    list_a = ak(data,lesson)
+    list_b = bk(data)
+    row = data.shape[0]
+    for i in range(row):
+        dependence_sum += list_a[i] * list_b[i]
+
+    print("The dependence coefficient between course '%s' and course 'Constitution' is:%f" %(lesson,dependence_sum))
+
+
+
+
+if __name__ =='__main__':
+    d = pd.read_csv('D:\大三上\数据挖掘\data.csv')
+    d = d.fillna(0)
+    length = len(d)   #学生人数
+    question_1(d,length)
+
+    question_2(d,length)
+
+    question_3(d,length)
+
+    question_4(d,'C1')
+    question_4(d,'C2')
+    question_4(d,'C3')
+    question_4(d,'C4')
+    question_4(d,'C5')
+    question_4(d,'C6')
+    question_4(d,'C7')
+    question_4(d,'C8')
+    question_4(d,'C9')
